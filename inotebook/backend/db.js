d77bca98bc352e1809c32bb8c8d28c47ed9146c5
leezakhanna <leezakhanna2109@gmail.com>
2023-06-17 00:06:50 +0530

    saving data in mongodb

---
+++ inotebook/backend/db.js
@@ -0,0 +1,12 @@
+const mongoose=require('mongoose')
+const mongoURI='mongodb+srv://leezakhanna2109:ohBw8CYZa12A4KON@cluster0.mcwh2li.mongodb.net/?retryWrites=true&w=majority'
+
+
+const connectToMongo=()=>{
+    mongoose.connect(mongoURI).then(()=>{
+        console.log("connection succesfull")
+    }).catch((err)=>{
+        console.log("no connection")
+    })
+}
+module.exports=connectToMongo;
---
+++ inotebook/backend/index.js
@@ -0,0 +1,23 @@
+const connectToMongo=require('./db')
+const express = require('express')
+connectToMongo();
+
+
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.use(express.json()) 
+app.use('/api/Auth', require('./routes/Auth'));
+app.use('/api/notes', require('./routes/notes'))
+ 
+  app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
