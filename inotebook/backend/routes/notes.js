22bbb64769b2a674b184694226b96f95279effc3
leezakhanna <leezakhanna2109@gmail.com>
2023-06-20 11:31:35 +0530

    delete note

--- inotebook/backend/routes/notes.js
+++ inotebook/backend/routes/notes.js
@@ -6,10 +6,6 @@ const {Schema}=mongoose;
 const Note = require('../models/Notes');
 const { body, validationResult } = require('express-validator');
 
-// router.get('/', (req, res)=>{
-
-//     res.json([])
-// } )
 // ROUTE 1: Get All the Notes using: GET "/api/auth/getuser". Login required
 router.get('/fetchallnotes', fetchuser, async (req, res) => {
     try {
@@ -68,5 +64,25 @@ router.put('/updatenote/:id', fetchuser, async (req, res) => {
     res.json({note});
 
     })
+
+    // ROUTE 4: Delete an existing Note using: DELETE "/api/notes/deletenote". Login required
+router.delete('/deletenote/:id', fetchuser, async (req, res) => {
+    try {
+        // Find the note to be delete and delete it
+        let note = await Note.findById(req.params.id);
+        if (!note) { return res.status(404).send("Not Found") }
+
+        // Allow deletion only if user owns this Note
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).send("Not Allowed");
+        }
+
+        note = await Note.findByIdAndDelete(req.params.id)
+        res.json({ "Success": "Note has been deleted", note: note });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+})
 
 module.exports = router
