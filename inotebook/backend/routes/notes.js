a80e184611482edd45efbf040cefee3fcf0d69a0
leezakhanna <leezakhanna2109@gmail.com>
2023-06-20 11:35:52 +0530

    delete note

--- inotebook/backend/routes/notes.js
+++ inotebook/backend/routes/notes.js
@@ -47,6 +47,7 @@ router.post('/addnote', fetchuser, [
 router.put('/updatenote/:id', fetchuser, async (req, res) => {
     const {title, description, tag} = req.body;
     // Create a newNote object
+    try {
     const newNote  = {};
     if(title){newNote.title = title};
     if(description){newNote.description = description};
@@ -56,14 +57,16 @@ router.put('/updatenote/:id', fetchuser, async (req, res) => {
     let note = await Note.findById(req.params.id);
     if(!note){return res.status(404).send("Not Found")}
 
-    if(note.user.toString() !== req.user.id){
+    if (note.user.toString() !== req.user.id) {
         return res.status(401).send("Not Allowed");
     }
-
-    note = await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new:true})
-    res.json({note});
-
-    })
+    note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
+    res.json({ note });
+} catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+}
+})
 
     // ROUTE 4: Delete an existing Note using: DELETE "/api/notes/deletenote". Login required
 router.delete('/deletenote/:id', fetchuser, async (req, res) => {
